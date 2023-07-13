6131b8a5d3160fed4b34ec818a62a0e44587831c
Georgii Selivirov <gselivirov@gmail.com>
2023-07-13 17:23:02 +0400

    initial commit

---
+++ tasks/models.py
@@ -0,0 +1,11 @@
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db import models
+
+class Tasks(models.Model):
+    user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
+    title = models.TextField(max_length=50)
+    text = models.TextField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField(blank=True)
+    # status = models.CharField()
