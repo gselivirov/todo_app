12f5e40529ecf23eb92dbf14f7ac81e14b86b67f
Georgii Selivirov <gselivirov@gmail.com>
2023-07-13 17:46:47 +0400

    create task model and view

--- tasks/models.py
+++ tasks/models.py
@@ -2,10 +2,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 
-class Tasks(models.Model):
+
+class Task(models.Model):
     user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
     title = models.TextField(max_length=50)
     text = models.TextField(max_length=200)
     created_at = models.DateTimeField(default=timezone.now)
-    due_date = models.DateTimeField(blank=True)
+    due_date = models.DateTimeField(blank=True, null=True)
     # status = models.CharField()
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Task
+
+
+class TaskListView(View):
+    model_class = Task
+
+    def get(self, request, *args, **kwargs):
+        task = self.model_class.objects.get(pk=1)
+        return render(request, "tasks/task_list.html", {"title": task.title})
