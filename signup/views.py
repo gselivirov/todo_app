6780d13cd65582b1ab42efcbb5ea8a07c68740b6
Georgii Selivirov <gselivirov@gmail.com>
2023-07-14 17:02:12 +0400

    Add handling POST requests by SignupView

--- signup/views.py
+++ signup/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import SignupForm
 
+
 class SignupView(View):
     form_class = SignupForm
-    # def post(self, request, *args, **kwargs):
-        
+    template_name = "signup/signup.html"
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        return render(request, self.template_name, {"form": form})
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
-        return render(request, "signup/signup.html", {"form": form})
+        return render(request, self.template_name, {"form": form})
 
 
 # class LoginView(View):
