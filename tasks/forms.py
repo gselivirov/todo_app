7051db4e774e59ee03ba74cf5635e10a4767894f
Georgii Selivirov <gselivirov@gmail.com>
2023-07-18 13:29:14 +0400

    Add status selection to Task model

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,7 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "text", "due_date",]
+        fields = ["title", "text", "due_date", "status"]
         widgets = {
             "title": forms.widgets.TextInput(),
             "due_date": forms.widgets.DateTimeInput(),
--- tasks/models.py
+++ tasks/models.py
@@ -4,12 +4,20 @@ from django.db import models
 
 
 class Task(models.Model):
+    LOW_PRIORITY = "low_priority"
+    HIGH_PRIORITY = "high_priority"
+    STATUS_CHOICES = [
+        (LOW_PRIORITY, "Low priority"),
+        (HIGH_PRIORITY, "High priority"),
+    ]
+
+
     user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
     title = models.TextField(max_length=50)
     text = models.TextField(max_length=200)
     created_at = models.DateTimeField(default=timezone.now)
     due_date = models.DateTimeField(blank=True, null=True)
-    # status = models.CharField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=LOW_PRIORITY)
 
     def __str__(self):
         return str(self.title)
